f9d6e5bb538432e9359c22a8a397c78786cd45ec
Chris Keller <christopherlawrencekeller@gmail.com>
2012-07-03 20:24:53 -0500

    Removed easy_maps as an app dependency…

--- easy_maps/admin.py
+++
@@ -1,9 +0,0 @@
-from easy_maps.models import Address
-from django.contrib import admin
-
-class AddressAdmin(admin.ModelAdmin):
-    list_display = ['address', 'computed_address', 'latitude', 'longitude', 'geocode_error']
-    list_filter = ['geocode_error']
-    search_fields = ['address']
-
-admin.site.register(Address, AddressAdmin)
