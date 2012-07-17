04396e76facd5814496f5b9df8fdc8f3b79abb27
Chris Keller <christopherlawrencekeller@gmail.com>
2012-07-17 08:10:32 -0500

    Added filter to main index view…
    On main incident list, I've added a filter so only incidents in the
    last week will show on the map and in the table. Prepping to add
    additional filtering options.

--- madcrime/views.py
+++ madcrime/views.py
@@ -1,4 +1,5 @@
-# Create your views here.
+# Create your views here
+from datetime import datetime, date, time, timedelta
 from django.shortcuts import get_object_or_404, render_to_response
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
@@ -6,12 +7,16 @@ from django.core import serializers
 from django.template import RequestContext
 from madcrime.models import Incident
 
-#the main index request
+# the main index request
+# filter incidents from the last week
 def index(request):
-    incident_listing = Incident.objects.all().order_by('-dateDetails', 'typeDetails')
-    return render_to_response('incident-table.html', {'incident_listing': incident_listing})
+   startdate = date.today()
+   enddate = timedelta(days=7)
+   displaydate = startdate - enddate
+   incident_listing = Incident.objects.filter(dateDetails__gte=displaydate)
+   return render_to_response('incident-table.html', {'incident_listing': incident_listing})
         
-#details request
+# details request
 def detail(request, incident_id):
     p = get_object_or_404(Incident, pk=incident_id)
     return render_to_response('details-table.html', {'incident': p},
