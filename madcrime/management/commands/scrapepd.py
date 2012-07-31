235c8bc83cb4e9d7ba0cff4d9373f8ddebdc74ac
Chris Keller <christopherlawrencekeller@gmail.com>
2012-07-30 23:22:18 -0500

    Scraper update...
    It's likely not the prettiest method, but added check against database
    for case number when scraping. I was losing addresses I had edited when
    I ran the scraper.

--- madcrime/management/commands/scrapepd.py
+++ madcrime/management/commands/scrapepd.py
@@ -12,10 +12,10 @@ from dateutil import parser
 
 class Command(BaseCommand):
     help = 'Scrapes Madison Police Department Data'
-
+    
     def handle(self, *args, **options):
         pageNumber = raw_input('Which Page to Scrape?')
-
+        
         print ('\nStarting scrape at %s\n' % str(datetime.datetime.now()))
         
         # set some varibles to make life easier
@@ -37,16 +37,16 @@ class Command(BaseCommand):
         lengthIncidents = len(rowsIncidents)
         
         for row in rowsIncidents:
-                
+        
             # whittle through content
             cells = row.findAll('td')
             dateIncidents = cells[0].string
-
+            
             # use dateutil to convert string to date/time object
             convertedDateIncidents = parser.parse(dateIncidents)
             linkIncidents = prefixIncidents + cells[1].a['href']
             caseIncidents  = cells[2].text.encode('utf-8')
-
+            
             # save content to django models
             try:
                 obj = Incident.objects.get(caseIncidents = caseIncidents)
@@ -57,98 +57,102 @@ class Command(BaseCommand):
             except Incident.DoesNotExist:
                 obj = Incident(dateIncidents = convertedDateIncidents, linkIncidents=linkIncidents, caseIncidents = caseIncidents)
                 obj.save()
-                            
-            # use selectors to whittle
-            # down to the content
-            mech = Browser()
-            urlDetails = linkIncidents
-            pageDetails = mech.open(urlDetails)
-            htmlDetails = pageDetails.read()
-            soupDetails = BeautifulSoup(htmlDetails, convertEntities=BeautifulSoup.HTML_ENTITIES)
-
-            # replaces br tags with periods
-            for breakPoint in soupDetails.findAll('br'):
-                new = "."
-                breakPoint.replaceWith(new)
-            
-            # hits incident detail table and returns the contents
-            tableDetails = soupDetails.find('table', {'id': 'incidentdetail'})
-            
-            # finds the tbody that contains the data
-            bodyDetails = tableDetails.find('tbody')
-        
-            # find all of the rows in the table
-            rowsDetails = bodyDetails.findAll('tr')
-
-            # create an empty dict
-            models = {}
-
-            # determine how many rows in the table
-            lengthDetails = len(rowsDetails)
-        
-            # set the count
-            countDetails = 0
-        
-            # each row do this
-            while (countDetails < lengthDetails):
-               for item in rowsDetails:
-               
-                   # create pairs
-                   labelDetails = item.find('th').text
-                   dataDetails = item.find('td').text
-                   
-                   # add to dict
-                   models[labelDetails] = dataDetails
-                   
-                   # repeat loop
-                   countDetails = countDetails + 1
-               
-               #print models
-               if models.has_key('Incident Date'):
-                   dateDetails = models['Incident Date']
-                   convertedDateDetails = parser.parse(dateDetails)
-                   obj.dateDetails = convertedDateDetails
-                   obj.save()
-                   
-               if models.has_key('Incident Type'):
-                   typeDetails = models['Incident Type']
-                   obj.typeDetails = typeDetails
-                   obj.save()
-                   
-               if models.has_key('Address'):
-                   addressDetails = models['Address']
-                   obj.addressDetails = addressDetails + addressSuffix
-                   obj.save()
                 
-               if models.has_key('Suspect(s)'):
-                   suspectDetails = models['Suspect(s)']
-               else:
-                   suspectDetails = 'None'
+                
+                # down to the content
+                # use selectors to whittle
+                mech = Browser()
+                urlDetails = linkIncidents
+                pageDetails = mech.open(urlDetails)
+                htmlDetails = pageDetails.read()
+                soupDetails = BeautifulSoup(htmlDetails, convertEntities=BeautifulSoup.HTML_ENTITIES)
+                
+                # replaces br tags with periods
+                for breakPoint in soupDetails.findAll('br'):
+                    new = "."
+                    breakPoint.replaceWith(new)
+                
+                # hits incident detail table and returns the contents
+                tableDetails = soupDetails.find('table', {'id': 'incidentdetail'})
+                
+                # finds the tbody that contains the data
+                bodyDetails = tableDetails.find('tbody')
+                
+                # find all of the rows in the table
+                rowsDetails = bodyDetails.findAll('tr')
+                
+                # create an empty dict
+                models = {}
+                
+                # determine how many rows in the table
+                lengthDetails = len(rowsDetails)
+                
+                # set the count
+                countDetails = 0
+                
+                # each row do this
+                while (countDetails < lengthDetails):
+                    for item in rowsDetails:
+                        # create pairs
+                        labelDetails = item.find('th').text
+                        dataDetails = item.find('td').text
+                        
+                        # add to dict
+                        models[labelDetails] = dataDetails
+                        
+                        # repeat loop
+                        countDetails = countDetails + 1
+                        
+                        #print models
+                        if models.has_key('Incident Date'):
+                            dateDetails = models['Incident Date']
+                            convertedDateDetails = parser.parse(dateDetails)
 
-               # adds space after period
-               p = re.compile('(\S)\.(\S)')
-               suspectOutput = re.sub(p, '\g<1>. \g<2>', suspectDetails)
+                        if models.has_key('Incident Type'):
+                            typeDetails = models['Incident Type']
 
-               obj.suspectDetails = suspectOutput
-               obj.save()
+                        if models.has_key('Address'):
+                            addressDetails = models['Address']
 
-               if models.has_key('Arrested'):
-                   arrestedDetails = models['Arrested']
+                        if models.has_key('Suspect(s)'):
+                            suspectDetails = models['Suspect(s)']
+                        else:
+                            suspectDetails = 'None'
+                        
+                        # adds space after period
+                        p = re.compile('(\S)\.(\S)')
+                        suspectOutput = re.sub(p, '\g<1>. \g<2>', suspectDetails)
+                        
+                        if models.has_key('Arrested'):
+                            arrestedDetails = models['Arrested']
+                        else:
+                            arrestedDetails = 'None'
+                        
+                        # adds space after period
+                        p = re.compile('(\S)\.(\S)')
+                        arrestOutput = re.sub(p, '\g<1>. \g<2>', arrestedDetails)
+                        
+                        if models.has_key('Victim'):
+                            victimDetails = models['Victim']
+                        
+                        if models.has_key('Details'):
+                            detailsDetails = models['Details']
+                            
+                            try:
+                                obj = Incident.objects.get(caseIncidents = caseIncidents)
+                                obj.dateDetails = convertedDateDetails
+                                obj.dateIncidents = convertedDateIncidents
+                                obj.typeDetails = typeDetails
+                                obj.addressDetails = addressDetails
+                                obj.suspectDetails = suspectDetails
+                                obj.arrestedDetails = arrestedDetails
+                                obj.victimDetails = victimDetails
+                                obj.detailsDetails = detailsDetails[:3000]
+                                obj.linkIncidents = linkIncidents
+                                obj.caseIncidents = caseIncidents
+                                obj.save()
+                            except Incident.DoesNotExist:
+                                obj = Incident(dateDetails = convertedDateDetails, dateIncidents = convertedDateIncidents, typeDetails = typeDetails, addressDetails = addressDetails, suspectDetails = suspectDetails, arrestedDetails = arrestedDetails, victimDetails = victimDetails, detailsDetails = detailsDetails[:3000], linkIncidents = linkIncidents, caseIncidents = caseIncidents)
+                                obj.save()
 
-                   # adds space after period
-                   p = re.compile('(\S)\.(\S)')
-                   arrestOutput = re.sub(p, '\g<1>. \g<2>', arrestedDetails)
-                   obj.arrestedDetails = arrestOutput
-                   obj.save()
-
-               if models.has_key('Victim'):
-                   victimDetails = models['Victim']
-                   obj.victimDetails = victimDetails
-                   obj.save()
-                   
-               if models.has_key('Details'):
-                   detailsDetails = models['Details']
-                   obj.detailsDetails = detailsDetails[:3000]
-                   obj.save()
-               
         print 'Finished scraping page ' + pageNumber + ' of Madison Police Incidents.'
